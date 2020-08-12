d1ac97ffe994014ac2ede9462aa7b37669e56d47
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-12 11:54:37 -0400

    handlebars files set up

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,3 +3,24 @@ const express = require("express");
 const router = express.Router();
 
 const burger = require('../models/burger.js');
+
+router.get('/', (req, res) => {
+    burger.all((data) => {
+        let obj = {
+            burgers: data
+        };
+        console.log(obj);
+        res.render('index', obj);
+    });
+});
+router.post('api/burgers', (req, res) => {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], (result) =>{
+        res.json( { BLANK });
+    });
+});
+router.put('/api/burgers/:devoured', (req, res) => {
+    let condition = "devoured = " + req.params.devoured;
+    burger.update
+})
+
+module.exports = router;
