d6a924d074bb0e63d7d6a0f3ac8c2e0fdcfbd0f6
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-15 16:10:36 -0400

    process.env

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: process.env.PORT| 3306,
   user: "root",
   password: "new_password",
   database: "burgers_db"
